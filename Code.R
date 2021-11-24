18bc163b1d8437b1650bf2d003a2386a63d49901
Annapurani93 <51084584+Annapurani93@users.noreply.github.com>
2021-11-24 11:48:39 +0530

    Create Code.R

---
+++ Code.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(tidytuesdayR)
+tuesdata <- tidytuesdayR::tt_load('2021-11-23')
+tuesdata$writers->writers
+tuesdata$directors->directors
+tuesdata$episodes->episodes
+tuesdata$imdb->imdb
+
+glimpse(imdb)
+
+imdb%>%
+  select(season,ep_num,rating,rating_n)%>%
+  group_by(season,ep_num)%>%
+  arrange(rating, rating_n,.by_group = TRUE)%>%
+  data.frame()->data
+
+
+ggplot(data,aes(x=as.factor(season),y=as.factor(ep_num),fill=rating,size=rating_n))+
+  geom_point(aes(fill=rating),pch=21,colour="gray75")+
+  scale_fill_distiller(palette="RdYlGn",limits=c(3,10),direction=1)+
+  scale_size_continuous(limits=c(2000,20000))+
+  labs(fill="Rating: ",size="Number of Ratings: ")+
+  theme(plot.margin=unit(c(0.5,1.5,0.5,1.5),"cm"),
+        plot.background = element_rect(fill="black"),
+        panel.background = element_rect(fill="black"),
+        panel.grid = element_line(colour="gray10"),
+        axis.text = element_text(colour="white",size=10,face="bold"),
+        axis.title.x = element_text(colour="white",size=10, face="bold",margin=margin(t=15)),
+        axis.title.y = element_text(colour="white",size=10, face="bold",margin=margin(r=15)),
+        legend.background = element_rect(fill="black"),
+        legend.key = element_rect(fill="black"),
+        legend.text=element_text(colour="white",margin=margin(l=15),face="bold"),
+        legend.key.height = unit(1,"cm"),
+        legend.box = "vertical",
+        legend.title = element_text(colour="white",face="bold",size=12,margin=margin(b=15)),
+        legend.position = "right",
+        plot.title.position = "plot",
+        plot.caption.position = "plot",
+        panel.border = element_rect(fill=NA,colour = "gray50"),
+        plot.title=element_text(size=14, face="bold",colour="white",margin=margin(b=15)),
+        plot.subtitle = element_text(size=12, colour="white",margin=margin(b=25)),
+        plot.caption=element_text(size=10,colour="white",hjust=0,margin=margin(t=20)))+
+  labs(title="DOCTOR WHO: HIGHEST-RATED EPISODES OF EACH SEASON",
+       subtitle="The ratings are for each episode are encoded by colour and the size of the dot denotes the number of ratings",
+       caption = "Data via Tidy Tuesday| Analysis and design: @annapurani93")+
+  xlab("-------------SEASONS---------------")+
+  ylab("-------------EPISODES--------------")->plot
+
+ggsave("doctorwhoheatmap.png",width=11,height=7)
+
